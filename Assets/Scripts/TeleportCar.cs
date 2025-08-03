a5e4103a8ff7e11b911857639c1abdaed93b5471
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2025-08-03 02:14:42 -0400

    f

---
+++ Assets/Scripts/TeleportCar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCar : MonoBehaviour
+{
+    #region Public Variables
+    public Transform targetDestination;
+    #endregion
+
+    #region Functions
+    private void OnTriggerEnter(Collider other)
+    {
+        CarController car = other.gameObject.GetComponent<CarController>();
+        if (car != null)
+        {
+            car.transform.position = targetDestination.position;
+            car.transform.rotation = targetDestination.rotation;
+        }
+    }
+    #endregion
+}
